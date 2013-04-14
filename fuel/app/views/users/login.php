006eff89088661e05d8c640a1b36531b2e10179d
Liz <wizzah@gmail.com>
2013-04-14 15:57:04 -0400

    Changed template.php stuff to be in welcome/index instead

--- fuel/app/views/users/login.php
+++ fuel/app/views/users/login.php
@@ -1,3 +1,32 @@
 <p>Login</p>
 <?php if (isset($errors)){echo $errors;}?>
 <?php echo $form; ?>
+
+
+<div class="container">
+
+	<!-- Load in default match if ($match) -->
+
+	<div id="menu">
+		<ul class="tempDashboard">
+			<li> 
+				<form>
+					Skip to: <input type="text" name="skip" id="skip" /><input type="submit" value="Go" id="submit" />
+				</form>
+			</li>
+		</ul>
+		<ul class="tempDashboard">
+			<li>Select Card</li>
+			<li>Spacebar</li>
+		</ul>
+		<ul class="tempDashboard">
+			<li>Select Cards</li>
+			<li>Arrow keys</li>
+		</ul>
+		<ul class="tempDashboard">
+			<li>Skip to Card Number</li>
+			<li>Ctrl</li>
+		</ul>
+	</div>
+
+</div>
